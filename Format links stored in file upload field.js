7cc07c0db8bc65e856a2a640dafd28418bd77242
rebwong <123711441+rebwong@users.noreply.github.com>
2023-08-08 12:46:13 +0800

    Update and rename Format links stored in file upload field to Format links stored in file upload field.js

--- Format links stored in file upload field.js
+++ Format links stored in file upload field.js
@@ -4,24 +4,24 @@ exports.main = async (event, callback) => {
   *****/
   const uploadLinks = event.inputFields['file_upload'];
  
-// Function to split string of links
- function splitStr(str) {
-	return str.split(";");
-}
-
-// Store split links in an array 
-const array = splitStr(uploadLinks);
-  
-// Prepare HTML blob
-let innerbody = "";
-
-for (let i = 0; i < array.length; i++) {
-	innerbody += "<li> <a href='"  + array[i] + "'>" + "File " + [i+1] + "<\a></li>";
-}  
-  
-const HTMLresult = "<ul>" + innerbody + "</ul>"
-  	
-console.log(HTMLresult);
+	// Function to split string of links
+	 function splitStr(str) {
+		return str.split(";");
+	}
+	
+	// Store split links in an array 
+	const array = splitStr(uploadLinks);
+	  
+	// Prepare HTML blob
+	let innerbody = "";
+	
+	for (let i = 0; i < array.length; i++) {
+		innerbody += "<li> <a href='"  + array[i] + "'>" + "File " + [i+1] + "<\a></li>";
+	}  
+	  
+	const HTMLresult = "<ul>" + innerbody + "</ul>"
+	  	
+	console.log(HTMLresult);
 
   /*****
     Use the callback function to output data that can be used in later actions in your workflow.
